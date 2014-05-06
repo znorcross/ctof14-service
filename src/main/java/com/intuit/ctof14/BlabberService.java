d363489a7e014692de739fc43104f78d5f8d3ab7
jefferyharlowbrewer@gmail.com <>
2014-05-06 07:58:07 +0000

    Solutions

--- src/main/java/com/intuit/ctof14/BlabberService.java
+++ src/main/java/com/intuit/ctof14/BlabberService.java
@@ -1,16 +1,11 @@
 package com.intuit.ctof14;
 
-import java.net.URLEncoder;
-
-import oauth.signpost.AbstractOAuthConsumer;
 import oauth.signpost.OAuthConsumer;
 import com.intuit.ctof14.util.UnirestoAuthConsumer;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.request.HttpRequestWithBody;
 import com.mashape.unirest.request.body.MultipartBody;
-import com.mashape.unirest.request.body.RequestBodyEntity;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
--- src/main/java/com/intuit/ctof14/ReviewService.java
+++ src/main/java/com/intuit/ctof14/ReviewService.java
@@ -1,34 +1,46 @@
 package com.intuit.ctof14;
 
+import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 
+import com.intuit.ctof14.ReviewService;
 import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("v1")
-@Api(value="v1/review", description="Review API - this service stores and returns reviews and ratings")
+@Path("v1/reviews")
+@Api(value="v1/reviews", description="Review API - this service stores and returns reviews and ratings")
 public class ReviewService
 {
-  @Path("review/{id}")
+  @Path("/review/{id}")
   @GET
   @Produces("application/json")
-  public Response review(@QueryParam("id") String id) {
-    InputStream stream = this.getClass().getResourceAsStream("reviews.json");
-    JSONTokener tokener = new JSONTokener(stream);
-    JSONObject reviews = new JSONObject(tokener);
-    
-    return Response.ok(reviews.get(id)).build();
+  @ApiOperation(value = "Get a Review", notes = "More notes about this method")
+  public Response review(@PathParam("id") String id) {
+    try {
+      InputStream stream = this.getClass().getResourceAsStream("/reviews.json");
+      StringWriter writer = new StringWriter();
+      IOUtils.copy(stream, writer, "UTF-8");
+      
+      JSONObject reviews = new JSONObject(writer.toString());
+      
+      return Response.ok(reviews.get(id).toString(), MediaType.APPLICATION_JSON_TYPE).build();
+    } catch (IOException ex) {
+      return Response.status(500).build();      
+    }
   }
   
-  @Path("dailyRating")
+  @Path("/dailyRating")
   @GET
   @Produces("application/json")
   public double getDailyRating() {
