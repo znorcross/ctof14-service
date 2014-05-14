1f4f490f5b79faad5736ae4939546c40e8859baa
Zackery B. Norcross <zackery_norcross@intuit.com>
2014-05-14 08:46:28 -0700

    Reformated code, also testing code envy sync

--- src/main/java/com/intuit/ctof14/TestServiceConsumer.java
+++ src/main/java/com/intuit/ctof14/TestServiceConsumer.java
@@ -19,90 +19,80 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("testConsumer")
-public class TestServiceConsumer
-{
-  final static String baseFeedUri = "https://activityfeed-prf.platform.intuit.com";
-  
-   @GET
-   @Path("findBestRestaurant")
-   @Produces("application/json")
-   public Response findBestRestaurant()
-   {
-      HttpRequest request = null;
-      try
-      {
-         // Setup request to gateway activity feed end point using the very handy 
-         // Unirest library http://unirest.io/
-         request =
-            Unirest.get(TestServiceConsumer.baseFeedUri+"/v1/feeds")
-              .header("accept", "application/json")
-              .header("Authorization", Application.getAuthorizationHeader());
+public class TestServiceConsumer {
+    final static String baseFeedUri = "https://activityfeed-prf.platform.intuit.com";
 
-         // Make request to gateway
-         HttpResponse<JsonNode> jsonResponse = request.asJson();
-         JsonNode body = jsonResponse.getBody();
+    @GET
+    @Path("findBestRestaurant")
+    @Produces("application/json")
+    public Response findBestRestaurant() {
+        HttpRequest request = null;
+        try {
+            // Setup request to gateway activity feed end point using the very handy
+            // Unirest library http://unirest.io/
+            request =
+                    Unirest.get(TestServiceConsumer.baseFeedUri + "/v1/feeds")
+                           .header("accept", "application/json")
+                           .header("Authorization", Application.getAuthorizationHeader());
 
-         // Get the array of feed elements
-         JSONArray feed = body.getObject().getJSONArray("feed");
-         
-         String winningRestaurant = "";
-         
-         // Iterate through feed list finding best restaurant
-         for (int index = 0; index < feed.length(); index++)
-         {
-            JSONObject item = feed.getJSONObject(index);
-            String itemContent = item.has("content") ? item.getString("content") : "";
-            if(itemContent.length() > winningRestaurant.length()) {
-               winningRestaurant = itemContent;
+            // Make request to gateway
+            HttpResponse<JsonNode> jsonResponse = request.asJson();
+            JsonNode body = jsonResponse.getBody();
+
+            // Get the array of feed elements
+            JSONArray feed = body.getObject().getJSONArray("feed");
+
+            String winningRestaurant = "";
+
+            // Iterate through feed list finding best restaurant
+            for (int index = 0; index < feed.length(); index++) {
+                JSONObject item = feed.getJSONObject(index);
+                String itemContent = item.has("content") ? item.getString("content") : "";
+                if (itemContent.length() > winningRestaurant.length()) {
+                    winningRestaurant = itemContent;
+                }
             }
-         }
-         
-         // Return winner
-         JSONObject obj = new JSONObject();
-         obj.put("result", winningRestaurant);
-         return Response.ok(obj.toString(), MediaType.APPLICATION_JSON).build();
-      }
-      catch (Throwable e)
-      {
-          return Response.status(500).build();
-      }
-   }
-   
-   @GET
-   @Path("postBestRestaurant")
-   @Produces("application/json")
-   public Response postBestRestaurant(@QueryParam("bestRestaurant") String bestRestaurant)
-   {
-      RequestBodyEntity request = null;
-      try
-      {
-        // Create a new note feed item
-        JSONObject feedItem = new JSONObject();
-        feedItem.put("content", "A Better " + bestRestaurant);
-        feedItem.put("feedType", "Note");
-        
-         // Setup request to gateway activity feed end point
-         // Set the body of the request to the json object
-         request =
-            Unirest.post(TestServiceConsumer.baseFeedUri+"/v1/feed")
-              .header("accept", "application/json")
-              .header("Authorization", Application.getAuthorizationHeader())
-              .header("Content-Type","application/json")
-              .body(feedItem.toString());
 
-         // Make request to gateway
-         HttpResponse<JsonNode> jsonResponse = request.asJson();
-         JsonNode body = jsonResponse.getBody();
+            // Return winner
+            JSONObject obj = new JSONObject();
+            obj.put("result", winningRestaurant);
+            return Response.ok(obj.toString(), MediaType.APPLICATION_JSON).build();
+        } catch (Throwable e) {
+            return Response.status(500).build();
+        }
+    }
 
-         // Return success
-         JSONObject obj = new JSONObject();
-         obj.put("result", "success");
-         return Response.ok(obj.toString(), MediaType.APPLICATION_JSON).build();
-      }
-      catch (Throwable e)
-      {
-          return Response.status(500).build();
-      }
-     
-   }
+    @GET
+    @Path("postBestRestaurant")
+    @Produces("application/json")
+    public Response postBestRestaurant(@QueryParam("bestRestaurant") String bestRestaurant) {
+        RequestBodyEntity request = null;
+        try {
+            // Create a new note feed item
+            JSONObject feedItem = new JSONObject();
+            feedItem.put("content", "A Better " + bestRestaurant);
+            feedItem.put("feedType", "Note");
+
+            // Setup request to gateway activity feed end point
+            // Set the body of the request to the json object
+            request =
+                    Unirest.post(TestServiceConsumer.baseFeedUri + "/v1/feed")
+                           .header("accept", "application/json")
+                           .header("Authorization", Application.getAuthorizationHeader())
+                           .header("Content-Type", "application/json")
+                           .body(feedItem.toString());
+
+            // Make request to gateway
+            HttpResponse<JsonNode> jsonResponse = request.asJson();
+            JsonNode body = jsonResponse.getBody();
+
+            // Return success
+            JSONObject obj = new JSONObject();
+            obj.put("result", "success");
+            return Response.ok(obj.toString(), MediaType.APPLICATION_JSON).build();
+        } catch (Throwable e) {
+            return Response.status(500).build();
+        }
+
+    }
 }
